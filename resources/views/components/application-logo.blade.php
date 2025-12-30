e2d3bd173c076fe78ba212105d4ae76562c72784
M. Faris Rahmansyah <rahmansyahfaris@gmail.com>
2025-12-30 15:07:47 +0700

    feat(handover): Absolute Final - Auth Lockdown, Branding, and Deployment Ready

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div class="text-xl font-bold leading-tight text-gray-800 dark:text-gray-200">
     <span class="text-indigo-600">HSSE</span> LMS
     <br>
-    <span class="text-sm font-normal text-gray-500">EEES Company</span>
+    <span class="text-sm font-normal text-gray-500">EEES</span>
 </div>
